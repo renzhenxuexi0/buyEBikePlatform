be746dbead336c2fe97c45ed6f439358db0100fd
wuhaiyue <3084972826@qq.com>
2022-11-20 20:12:31 +0800

    创建项目

---
+++ src/main/java/com/curriculumdesign/buyebikeplatform/BuyEBikePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.curriculumdesign.buyebikeplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyEBikePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyEBikePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/curriculumdesign/buyebikeplatform/BuyEBikePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curriculumdesign.buyebikeplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyEBikePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
